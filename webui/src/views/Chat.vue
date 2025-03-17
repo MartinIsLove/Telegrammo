4ed1e6ef76b5aecd9664b7fdac59ac1c306bdaa0
Alessio <pernici.alessio12@gmail.com>
2025-03-17 19:37:49 +0000

    migliorato frontend chat

--- webui/src/views/Chat.vue
+++ webui/src/views/Chat.vue
@@ -26,12 +26,30 @@ export default {
             let response = await this.$axios.get(`/conversation/${this.chatId}`, {headers:{cs:this.id}});
 			this.messaggi = response.data
 
+			this.$nextTick(() => {
+                this.scrollToBottom();
+            });
         },
 		async send() {
-            let response = await this.$axios.post("/conversation/message", {id_chat:this.chatId, testo:this.message, photo:this.photo}, {headers: {cs:this.id}});
+            await this.$axios.post("/conversation/message", {id_chat:this.chatId, testo:this.message, photo:this.photo}, {headers: {cs:this.id}});
 
             this.fetchMessage()
 		},
+		formatDate(date) {
+            if (!date) return '';
+            const d = new Date(date);
+            return d.toLocaleDateString('it-IT', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+            });
+        },
+		scrollToBottom() {
+            const container = this.$refs.messageContainer;
+            if (container) {
+                container.scrollTop = container.scrollHeight;
+            }
+        }
 	},
 	mounted() {
 		this.refresh()
@@ -46,17 +64,39 @@ export default {
         <hr style="width: 100%;">
     </div>
 	<!-- <div style="height: 8%;"></div> -->
-
-	<div v-for="mes in messaggi.message" class="row g-0 border rounded my-2 p-2">
-		<div class="d-flex justify-content-between">
-			<p v-if="mes.gruppo" class="text-wrap">
-				<span class="text-capitalize fw-bold">{{ mes.nome }}</span>
-				{{ mes.testo }}
-			</p>
-			
-			<p v-else> 
-				<span>{{ mes.testo  }}</span>
-			</p>
+	<div ref="messageContainer"  style="overflow-y: auto; max-height: calc(100vh - 150px);">
+		<div v-for="mes in messaggi.message">
+			<div v-if="mes.testo === '' && mes.photo === null" class="d-flex justify-content-center my-2">
+				<div class="date-container">
+					<p class="text-wrap text-center mb-0">
+						{{ formatDate(mes.data) }}
+					</p>
+				</div>
+			</div>
+			<div v-else-if="id == mes.id_mitt" class="d-flex justify-content-end my-2 ">
+				<div class="message-container row g-0 border rounded p-2">
+					<div class="d-flex justify-content-between w-100">
+						<p v-if="mes.gruppo" class="text-wrap message-content">
+							<span class="text-capitalize fw-bold">{{ mes.nome }}</span><br>
+							{{ mes.testo }}
+						</p>
+						<p v-else class="message-content">
+							<span>{{ mes.testo }}</span>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div v-else class="row g-0 border rounded my-2 p-2 message-container">
+				<div class="d-flex justify-content-between w-100">
+					<p v-if="mes.gruppo" class="text-wrap message-content">
+						<span class="text-capitalize fw-bold">{{ mes.nome }}</span><br>
+						{{ mes.testo }}
+					</p>
+					<p v-else class="message-content">
+						<span>{{ mes.testo }}</span>
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
 	
@@ -75,3 +115,28 @@ export default {
   
     
 </template>
+<style>
+
+.text-center {
+    text-align: center;
+}
+.date-container {
+    display: inline-block;
+    padding: 10px; 
+    background-color: #1b1616;
+    border-radius: 5px; 
+}
+.message-container {
+    width: 40%; 
+    margin: 0 auto; 
+	
+}
+.message-content {
+    word-break: break-word; 
+    white-space: pre-wrap; 
+}
+body, html {
+    overflow: hidden; 
+    height: 100%; 
+}
+</style>
