565af64c3249ccf28890c92a96b8601170262b8f
Alessio <pernici.alessio12@gmail.com>
2025-05-16 22:13:47 +0000

    ricerca forward chat (wippy sei il mio idolo)

--- webui/src/views/Forward.vue
+++ webui/src/views/Forward.vue
@@ -19,33 +19,42 @@
             id_mes: null,
             searchCompleted: false,
             if_selection: false,
+
+            searchedChat: [],
+            searchedUser: [],
+
             selectedChat: [], 
             selectedUser: [],
 
 
-            allselectedchat: [],
             chats: [],
-            chChat: '',
+            search: '',
 
             error: '',
             message: '',
         }
     },
     watch: {
-        selectedChat(newVal) {
-            this.if_selection = newVal.length > 0 || this.selectedUser.length > 0;
-        },
-        selectedUser(newVal) {
-            this.if_selection = newVal.length > 0 || this.selectedChat.length > 0;
-        },
-        chChat(newVal) {
-            if (newVal === '') {
-                this.searchCompleted = false;
-                this.refresh(); 
-            } else {
-                this.searchCompleted = true;
+        search: function () {
+            const searchTerm = this.search.toLowerCase().trim();
+            if (!searchTerm) {
+                 this.searchedChat = this.chats.filter(chat => chat.id_chat !== 0 && chat.id_utenti !== 0);
+                this.searchedUser = this.chats.filter(chat => chat.id_chat === 0 || chat.id_utenti === 0);
+                return;
             }
-        }
+            // Filtra le chat e gli utenti in base al termine di ricerca
+            const filtered = this.chats.filter(chat => {
+                const name = (chat.nome || chat.username || '').toLowerCase();
+                return name.includes(searchTerm);
+            });
+
+            // Popola searchedChat e searchedUser in base ai risultati
+            this.searchedChat = filtered.filter(chat => chat.id_utenti !== 0);
+            this.searchedUser = filtered.filter(chat => chat.id_chat !== 0);
+
+
+
+        },
     },
     mounted() {
         this.id = sessionStorage.getItem("cs")
@@ -58,13 +67,17 @@
         async refresh() {
             this.loading = true;
             this.errormsg = null;
+
             this.id = sessionStorage.getItem("cs")
             this.id_chat = parseInt(this.$route.params.idchat, 10);
             this.id_mes = parseInt(this.$route.params.idmes, 10);
             this.id_utente= parseInt(this.$route.params.idutente, 10);
+
             try {
                 let response = await this.$axios.get("/conversations/forward", { headers: { cs: this.id } });
                 this.chats = response.data;
+                this.searchedChat = response.data.filter(chat => chat.id_chat !== 0 && chat.id_utenti !== 0);
+                this.searchedUser = response.data.filter(chat => chat.id_chat === 0 || chat.id_utenti === 0);
             } catch (e) {
                 this.errormsg = e.toString();
             }
@@ -72,14 +85,28 @@
         },
         
 
-        async handleSelectedChats() {
+        async forwardMessageToSelectedChats() {
             try {
                 
                 this.id = sessionStorage.getItem("cs")
                 
-                const selectedChatIds = this.selectedChat.map(chat => chat.id_chat);
-                const selectedUserIds = this.selectedUser.map(user => user.id_utenti);
-                let response = await this.$axios.post(`/message/forward`,{id_mes:this.id_mes, id_chat:selectedChatIds, id_utenti: selectedUserIds, id_for:this.id_utente} , { headers: { cs: this.id } });
+                const selectedChatIds = this.selectedChat
+                    .filter(chat => chat.id_chat !== 0)
+                    .map(chat => chat.id_chat);
+
+                console.log(this.selectedUser)
+                    
+                const selectedUserIds = this.selectedUser
+                    .filter(chat => chat.id_utenti !== 0)
+                    .map(chat => chat.id_utenti);
+
+                let response = await this.$axios.post(`/message/forward`,{
+                    id_mes: this.id_mes, 
+                    id_chat: selectedChatIds, 
+                    id_utenti: selectedUserIds, 
+                    id_for: this.id_utente
+                }, 
+                { headers: { cs: this.id } });
                 
                 if (response.status === 204) {
                     this.message = 'chat created';
@@ -88,20 +115,10 @@
                 }
             } catch (error) {
                 this.error = error
-                console.error('error create group user data: ', error)
+                console.error('forward error: ', error)
             }
         },
-        isSelected(chat) {
-            // Se è un gruppo/chat
-            if (chat.id_chat) {
-                return this.selectedChat.some(selected => selected.id_chat === chat.id_chat);
-            }
-            // Se è un utente
-            if (chat.id_utenti) {
-                return this.selectedUser.some(selected => selected.id_utenti === chat.id_utenti);
-            }
-            return false;
-        },
+ 
         remove(chat) {
             if (chat.id_chat) {
                 const index = this.selectedChat.findIndex(selected => selected.id_chat === chat.id_chat);
@@ -125,63 +142,26 @@
         <div class="d-flex">
             <form class="mt-3 flex-grow-1" @submit.prevent="getChats">
                 <label for="username" class="form-label">Search Chat</label>
-                <input type="text" class="form-control mb-2" id="username" placeholder="Insert Username" v-model="chChat" >
+                <input type="text" class="form-control mb-2" id="username" placeholder="Insert Username" v-model="search" >
             </form>
-            <button class="btn btn-secondary ms-1 mt-5 ml-auto h-100" @click="handleSelectedChats">
+            <button class="btn btn-secondary ms-1 mt-5 ml-auto h-100" @click="forwardMessageToSelectedChats">
                 Submit
             </button>
         </div>
     </div>
-    <div class="mt-3" v-if="if_selection">
-        <h5>Selected Chats:</h5>
-        <div v-for="chat2 in [...selectedChat, ...selectedUser]" :key="chat2.id_chat || chat2.id_utenti" class="selected-user">
+
+    
+    <div class="custom-scroll">
+        <div v-for="chat in [...searchedChat, ...searchedUser]" :key="chat.id_chat" class="form-check">
+            <input v-if="chat.id_chat > 0" class="form-check-input" type="checkbox" name="selectedUser" :value="chat" v-model="selectedChat">
+            <input v-if="chat.id_utenti > 0" class="form-check-input" type="checkbox" name="selectedUser" :value="chat" v-model="selectedUser">
             <div class="row g-0 border rounded my-2 p-2">
                 <div class="col-md-1 col-2">
-                    <img v-if="chat2.propic" :src="'data:image/png;base64,'+ chat2.propic" class="rounded-circle me-2" width="70" height="70" role="img" focusable="false" style="object-fit: cover;">
+                    <img v-if="chat.propic" :src="'data:image/png;base64,'+ chat.propic" class="rounded-circle me-2" width="70" height="70" role="img" focusable="false" style="object-fit: cover;">
                 </div>
                 <div class="col-md-11 col-10">
                     <div class="card-body">
-                        <h5>{{chat2.nome || chat2.username}}</h5>
-                    </div>
-                    <div class="col-md-1 col-2">
-                        <button class="btn btn-danger" @click="remove(chat2)">Remove</button>
-                    </div>
-                    
-                </div>
-                
-            </div>
-        </div>
-    </div>
-    <div v-if="searchCompleted">
-        <div class="custom-scroll">
-            <div v-for="chat in chats.filter(chat => !isSelected(chat))" :key="chat.id_chat" class="form-check">
-                <input class="form-check-input" type="checkbox" name="selectedUser" :value="chat" v-model="selectedChat">
-                <div class="row g-0 border rounded my-2 p-2">
-                    <div class="col-md-1 col-2">
-                        <img v-if="chat.propic" :src="'data:image/png;base64,'+ chat.propic" class="rounded-circle me-2" width="70" height="70" role="img" focusable="false" style="object-fit: cover;">
-                    </div>
-                    <div class="col-md-11 col-10">
-                        <div class="card-body">
-                            <h5>{{chat.nome || chat.username}}</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div v-else>
-        <div class="custom-scroll">
-            <div v-for="chat in chats.filter(chat => !isSelected(chat))" :key="chat.id_chat" class="form-check">
-                <input v-if="chat.id_chat != 0" class="form-check-input" type="checkbox" name="selectedUser" :value="chat" v-model="selectedChat">
-                <input v-if="chat.id_utenti != 0" class="form-check-input" type="checkbox" name="selectedUser" :value="chat" v-model="selectedUser">
-                <div  class="row g-0 border rounded my-2 p-2">
-                    <div class="col-md-1 col-2">
-                        <img v-if="chat.propic" :src="'data:image/png;base64,'+ chat.propic" class="rounded-circle me-2" width="70" height="70" role="img" focusable="false" style="object-fit: cover;">
-                    </div>
-                    <div class="col-md-11 col-10">
-                        <div class="card-body">
-                            <h5>{{chat.nome || chat.username}}</h5>
-                        </div>
+                        <h5>{{ chat.nome || chat.username }}</h5>
                     </div>
                 </div>
             </div>
